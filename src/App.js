8088738fb95bf92228b468cdc73ff7a51d88af4f
Jaffar Totanji <jacepac264@gmail.com>
2022-06-14 13:07:03 +0300

    itegrated metamask and added initial buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,76 @@
+import './App.css';
+import {useEffect, useState} from 'react';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
+
 function App() {
+
+  const [web3Api, setWeb3Api] = useState({
+    web3: null,
+    provider: null
+  });
+
+  const [account, setAccount] = useState(null);
+
+  useEffect(() =>
+  {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider
+        });
+
+      } else {
+        alert("Please install MetaMask.");
+      }
+    }
+
+    loadProvider();
+  }, [web3Api.web3])
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts(); 
+      setAccount(accounts[0]);
+    }
+
+
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
+  const enableEth = async () => {
+    web3Api.provider.request({method: "eth_requestAccounts"})
+  };
+
   return (
-    <div className="App">
-      Hi
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+        <div className="is-flex">
+            <span>
+              <strong className="mr-2 is-align-items-center">Account: </strong>
+            </span>
+              {account ? 
+              <div>{account}</div> : 
+              <button
+                className="button is-small"
+                onClick={enableEth}
+              >
+                Connect MetaMask
+              </button>
+              }
+          </div>
+          <div className="balance-view is-size-2 my-4">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="button is-link mr-2">Donate</button>
+          <button className="button is-primary">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
